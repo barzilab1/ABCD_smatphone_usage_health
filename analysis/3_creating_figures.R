6b3a10089baf8759d7f6ba347459c58d32b95cdf
Kate T Tran <trant13@chop.edu>
2026-03-31 22:24:38 -0400

    Update figures.

--- analysis/3_creating_figures.R
+++ analysis/3_creating_figures.R
@@ -27,7 +27,7 @@ theme_abcd <- function(base_size = 18) {
 
 
 #  FIGURE 1----
-
+## No need in the revision # May remove later
 df_aim1 <- tibble(
   Outcome = factor(c("Depression", "Obesity", "Insufficient Sleep"),
                    levels = c("Insufficient Sleep", "Obesity", "Depression")),
@@ -59,14 +59,13 @@ ggsave("plots/figure1.pdf", fig_aim1, width = 12, height = 8, dpi = 320)
 df_aim2 <- tibble(
   Outcome = factor(c("Depression", "Obesity", "Insufficient Sleep"),
                    levels = c("Insufficient Sleep", "Obesity", "Depression")),
-  OR      = c(1.34, 1.29, 1.35),
-  CI_low  = c(1.07, 0.96, 1.15),
-  CI_high = c(1.69, 1.73, 1.59),
-  N       = c(901, 797, 1006)
+  OR      = c(1.23, 1.46, 1.31),
+  CI_low  = c(1.01, 1.16, 1.14),
+  CI_high = c(1.50, 1.82, 1.50),
+  N       = c(1041, 1052, 1168)
 )
 
-
-fig_aim2a <- make_forest_plot(
+fig_aim2 <- make_forest_plot(
   df_aim2,
   x = OR,
   y = Outcome,
@@ -76,18 +75,19 @@ fig_aim2a <- make_forest_plot(
   min_x = 0.7,
   max_x = 2.05,
   x_breaks = seq(0.5, 2.0, 0.5),
-  title = "Total Smartphone-Time"
+  title = "Total Smartphone-Time",
+  y_label = "Health Outcome"
 )
 
 
-ggsave("plots/figure2.pdf", fig_aim2a, width = 12, height = 8, dpi = 320)
+ggsave("plots/figure2.pdf", fig_aim2, width = 12, height = 8, dpi = 320)
 
 
 #  FIGURE 3----
 df_depression_dos <- tribble(
   ~Category,  ~OR, ~CI_low, ~CI_high, ~N,
-  "2-5h/day",   1.61, 0.97, 2.66, 901,
-  ">5h/day",    2.87, 1.37, 6.04, 901
+  "2-5h/day",   1.48, 0.93, 2.37, 1041,
+  ">5h/day",    2.28, 1.16, 4.45, 1041
 ) %>%
   mutate(
     Category = factor(Category,
@@ -109,11 +109,36 @@ fig_depression_dos <- make_forest_plot(
 )
 
 
+df_obesity_dos <- tribble(
+  ~Category,  ~OR, ~CI_low, ~CI_high, ~N,
+  "2-5h/day",  1.19, 0.72, 1.99, 1052,
+  ">5h/day",   3.17, 1.58, 6.36, 1052
+) %>%
+  mutate(
+    Category = factor(Category,
+                      levels = c("2-5h/day", ">5h/day"))
+  )
+
+fig_obesity_dos <- make_forest_plot(
+  df_obesity_dos,
+  x = OR,
+  y = Category,
+  xmin = CI_low,
+  xmax = CI_high,
+  n_var = N,
+  min_x = 0.5,
+  max_x = 7,
+  x_breaks = c(0.5, 1, 2, 3, 4, 5, 6, 7),
+  title = "Obesity",
+  y_label = "Daily Smartphone Use Duration"
+)
+
+
 
 df_sleep_dos <- tribble(
   ~Category,  ~OR, ~CI_low, ~CI_high, ~N,
-  "2-5h/day",  1.62, 1.17, 2.23, 1006,
-  ">5h/day",   2.03, 1.22, 3.39, 1006
+  "2-5h/day",  1.66, 1.23, 2.24, 1168,
+  ">5h/day",   2.08, 1.33, 3.26, 1168
 ) %>%
   mutate(
     Category = factor(Category,
@@ -138,30 +163,33 @@ fig_sleep_dos <- make_forest_plot(
 spacer <- ggplot() + theme_void()
 
 row1 <- ggarrange(fig_depression_dos, labels = c("A"), font.label = list(size = 24, color = "black"))
-row2 <- ggarrange(fig_sleep_dos, labels = c("B"), font.label = list(size = 24, color = "black"))
+row2 <- ggarrange(fig_obesity_dos, labels = c("B"), font.label = list(size = 24, color = "black"))
+row3 <- ggarrange(fig_sleep_dos, labels = c("C"), font.label = list(size = 24, color = "black"))
 
 figure_3 <- ggarrange(
     row1,
     spacer,
     row2,
+    spacer,
+    row3,
     ncol = 1,
-    heights = c(1, 0.08, 1),
+    heights = c(1, 0.08, 1, 0.08, 1),
     align = "hv"
 )
 
 
-ggsave("plots/figure_3.pdf", figure_3, width = 12, height = 14, dpi = 320)
+ggsave("plots/figure_3.pdf", figure_3, width = 12, height = 20, dpi = 320)
 
 
 #  FIGURE 4----
 
 df_outside_room <- tibble(
-  Outcome = factor(c("Depression", "Insufficient Sleep"),
-                   levels = c("Insufficient Sleep", "Depression")),
-  OR      = c(0.97, 0.68),
-  CI_low  = c(0.56, 0.49),
-  CI_high = c(1.66, 0.95),
-  N       = c(896, 1002)
+  Outcome = factor(c("Depression", "Obesity", "Insufficient Sleep"),
+                   levels = c("Insufficient Sleep", "Obesity", "Depression")),
+  OR      = c(0.87, 0.86, 0.66),
+  CI_low  = c(0.52, 0.50, 0.48),
+  CI_high = c(1.45, 1.45, 0.90),
+  N       = c(1040, 1049, 1167)
 )
 
 fig_df_outside_room <- make_forest_plot(
@@ -174,7 +202,8 @@ fig_df_outside_room <- make_forest_plot(
   min_x = 0.45,
   max_x = 2.05,
   x_breaks = seq(0.5, 2.0, 0.5),
-  title = "Smartphone Out of Bedroom"
+  title = "Smartphone Out of Bedroom",
+  y_label = "Health Outcome"
 )
 
 
